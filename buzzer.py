3bfadbd75ac1e970c442b68c8a6e99b8e45b8879
datalore12 <jfanizzi@gmail.com>
2020-12-26 23:08:10 -0500

    modify buzzer.py

--- buzzer.py
+++ buzzer.py
@@ -6,6 +6,11 @@ bz=Buzzer(4)
 led = LED(17)
 
 led.on()
+# beep(on_time=1, off_time=1, n=None, background=True)
+# on_time = number of seconds on.  Defaults to 1 second
+# off_time = number of seconds off. Defaults to 1 second
+# n = number of times to buzz.  None (the default means forever)
+# https://gpiozero.readthedocs.io/en/stable/api_output.html
 bz.beep(0.5, 0.25, 8)
 time.sleep(5)
 led.off()
